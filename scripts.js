e9a292e347a1c805c8ac33d73bc33486b5c817d5
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2023-06-04 16:17:10 -0400

    Create calculation logic

--- scripts.js
+++ scripts.js
@@ -5,24 +5,73 @@ const divide = (a, b) => a / b;
 
 let num1 = 0;
 let num2 = 0;
-let operator = '+'
+let operator = "+";
 
 const operate = function (num1, num2, operator) {
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-            break;
-        case '-': {
-            return subtract(num1, num2);
-            break;
-        }
-        case '*': {
-            return multiply(num1, num2);
-            break;
-        }
-        case '÷':
-        case '/': {
-            return divide(num1, num2);
-        }
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-": {
+      return subtract(num1, num2);
+      break;
     }
+    case "*": {
+      return multiply(num1, num2);
+      break;
+    }
+    case "÷":
+    case "/": {
+      return divide(num1, num2);
+    }
+  }
+};
+
+const calc = function (nums, operators) {
+  while (operators.length > 0) {
+    let num1 = Number(nums.shift());
+    let num2 = Number(nums.shift());
+    let operator = operators.shift();
+    nums.unshift(operate(num1, num2, operator));
+  }
+  return nums[0];
+}
+
+// DOM
+let displayValue = "";
+let nums = [];
+let operators = [];
+
+const displayValueHTML = document.getElementById("calculator-display");
+
+const buttons = document.querySelectorAll("button");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    buttonValue = buttons[i].innerHTML;
+    switch (buttonValue) {
+      case "C":
+        displayValue = "";
+        displayValueHTML.innerHTML = "";
+        nums = [];
+        break;
+
+      case "=":
+        let spaceRemovedDisplay = displayValue.replaceAll(" ", "");
+        nums = spaceRemovedDisplay.split(/[+\-*\÷]/);
+
+        operators = [...spaceRemovedDisplay].filter((a) =>
+          ["+", "-", "*", "÷"].includes(a)
+        );
+
+        console.log(calc(nums, operators));
+        console.log(nums);
+        console.log(operators);
+        break;
+
+      default:
+        displayValue += `${buttonValue} `;
+        displayValueHTML.innerHTML = displayValue;
+    }
+  });
 }
